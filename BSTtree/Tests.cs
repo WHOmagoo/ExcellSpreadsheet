27d9678d115ca15c88fb7e072993b80163df7e65
hmcgough <hrmagoo@gmail.com>
2018-09-02 00:37:18 -0700

    Add test code

---
+++ BSTtree/Tests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using NUnit.Framework;
+using BSTtree;
+
+namespace BSTtreeTest
+{
+    [TestFixture]
+    public class Tests
+    {
+
+        [Test]
+        public void TestParseInput()
+        {
+            string input1 = "12 15 17 20 33 10";
+            int[] result1 = {12,15,17,20,33,10};
+
+            Assert.True(Enumerable.SequenceEqual(Program.parseInput(input1), result1));
+        }
+    }
+}                         
