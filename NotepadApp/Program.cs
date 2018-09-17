efce76a054bc6556a9dc9594bc798482cfe4d0ff
whomagoo <ros_MC_hrmcgough>
2018-09-17 11:17:25 -0700

    Initial commit

---
+++ NotepadApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NotepadApp
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("hello world.");
+        }
+    }
+}
