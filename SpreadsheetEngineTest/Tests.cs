e0457e2e6911976564ea4233bd6876cdb0765b33
whomagoo <ros_MC_hrmcgough>
2018-10-09 12:58:32 -0700

    add project for unit testing of spreadsheet engine.

---
+++ SpreadsheetEngineTest/Tests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using NUnit.Framework;
+using SpreadsheetEngine;
+
+namespace SpreadsheetEngineTest
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+
+        [Test]
+        public void TestCell()
+        {
+            Cell c = new Cell(4, 9);
+            Assert.True();
+        }
+    }
+}
