2d5dfad8eda63d6fa55fb684f00a32bd6e992c55
whomagoo <ros_MC_hrmcgough>
2018-10-09 12:57:14 -0700

    add code to test project configuration

--- Spreadsheet/Program.cs
+++ Spreadsheet/Program.cs
@@ -1,9 +1,16 @@
-﻿namespace Spreadsheet
+﻿using System;
+using SpreadsheetEngine;
+
+namespace Spreadsheet
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
+            Class1 test = new Class1(5);
+
+            Console.WriteLine(test.getVal());
+            
         }
     }
 }
