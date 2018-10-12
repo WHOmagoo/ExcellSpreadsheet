ec95e1430c8a5a48c5eac49deb6b1b58764826e3
hmcgough <hrmagoo@gmail.com>
2018-10-11 22:12:51 -0700

    Add test code for HeaderConverter from string to int

--- SpreadsheetEngineTest/Tests.cs
+++ SpreadsheetEngineTest/Tests.cs
@@ -10,7 +10,7 @@ namespace SpreadsheetEngineTest
         public void TestSimpleCell()
         {
             
-            SimpleCell c = new SimpleCell(4, 9);
+            SimpleCell c = new SimpleCell(9, 4);
             
             Assert.True(c.RowIndex == 4);
             Assert.True(c.ColIndex == 9);
@@ -18,7 +18,7 @@ namespace SpreadsheetEngineTest
         }
 
         [Test]
-        public void TestHeaderConverter()
+        public void TestHeaderConverterStringToChar()
         {
             Assert.AreEqual("A", HeaderConverter.Convert(0));
             Assert.AreEqual("I", HeaderConverter.Convert(8));
@@ -29,6 +29,19 @@ namespace SpreadsheetEngineTest
             Assert.AreEqual("BA", HeaderConverter.Convert(52));
             Assert.AreEqual("ZZ", HeaderConverter.Convert(701));
             Assert.AreEqual("AAA", HeaderConverter.Convert(702));
+        }
+
+        [Test]
+        public void TestHeaderConverterCharToString()
+        {
+           Assert.AreEqual(0, HeaderConverter.Convert("A")); 
+           Assert.AreEqual(8, HeaderConverter.Convert("I")); 
+           Assert.AreEqual(25, HeaderConverter.Convert("Z")); 
+           Assert.AreEqual(26, HeaderConverter.Convert("AA")); 
+           Assert.AreEqual(27, HeaderConverter.Convert("AB")); 
+           Assert.AreEqual(51, HeaderConverter.Convert("AZ")); 
+           Assert.AreEqual(52, HeaderConverter.Convert("BA")); 
+           Assert.AreEqual(702, HeaderConverter.Convert("AAA")); 
         }
     }
 }
