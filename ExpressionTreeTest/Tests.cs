f8d1780976b7fee1dfa7a7e072dbdd0ee6487e98
hmcgough <hrmagoo@gmail.com>
2018-10-22 05:18:30 -0700

    Refactor test code for ExpressionTrees to its own solution

---
+++ ExpressionTreeTest/Tests.cs
@@ -0,0 +1,37 @@
+﻿using CptS321;
+using NUnit.Framework;
+
+namespace ExpressionTreeTest
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void TestExpTree()
+        {
+            Assert.AreEqual(12, new ExpTree("3*4").Eval());
+            Assert.AreEqual(7, new ExpTree("3+4").Eval());
+            Assert.AreEqual(-1, new ExpTree("3-4").Eval());
+            Assert.AreEqual(2, new ExpTree("8/4").Eval());
+            
+        }
+
+        [Test]
+        public void TestExpTreeVariable()
+        {
+            ExpTree exp = new ExpTree("varA+varB+nd4spd");
+            exp.SetVar("varA", 1);
+            exp.SetVar("varB", 2);
+            exp.SetVar("nd4spd", 3);
+            
+            Assert.AreEqual(6, exp.Eval());
+            
+            exp = new ExpTree("-varA*varB*-nd4spd");
+            exp.SetVar("varA", -1);
+            exp.SetVar("varB", 2);
+            exp.SetVar("nd4spd", -3);
+            
+            Assert.AreEqual(6, exp.Eval());
+        }
+    }
+}
--- SpreadsheetEngineTest/Tests.cs
+++ SpreadsheetEngineTest/Tests.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using CptS321;
-using NUnit.Framework;
+﻿using NUnit.Framework;
 using SpreadsheetEngine;
 
 namespace SpreadsheetEngineTest
@@ -44,34 +42,6 @@ namespace SpreadsheetEngineTest
            Assert.AreEqual(51, HeaderConverter.Convert("AZ")); 
            Assert.AreEqual(52, HeaderConverter.Convert("BA")); 
            Assert.AreEqual(702, HeaderConverter.Convert("AAA")); 
-        }
-
-        [Test]
-        public void TestExpTree()
-        {
-            Assert.AreEqual(12, new ExpTree("3*4").Eval());
-            Assert.AreEqual(7, new ExpTree("3+4").Eval());
-            Assert.AreEqual(-1, new ExpTree("3-4").Eval());
-            Assert.AreEqual(2, new ExpTree("8/4").Eval());
-            
-        }
-
-        [Test]
-        public void TestExpTreeVariable()
-        {
-            ExpTree exp = new ExpTree("varA+varB+nd4spd");
-            exp.SetVar("varA", 1);
-            exp.SetVar("varB", 2);
-            exp.SetVar("nd4spd", 3);
-            
-            Assert.AreEqual(6, exp.Eval());
-            
-            exp = new ExpTree("-varA*varB*-nd4spd");
-            exp.SetVar("varA", -1);
-            exp.SetVar("varB", 2);
-            exp.SetVar("nd4spd", -3);
-            
-            Assert.AreEqual(6, exp.Eval());
         }
     }
 }
